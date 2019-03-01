1ade49c952881ae2a86f82d9488766d95b970c75
nsjethani <neha.jethani6627@gmail.com>
2019-03-01 15:51:03 -0800

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import Logo from './../assets/images/UABT_logo.png';
-import {withRouter} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 import './../assets/stylesheets/bootstrap.min.css';
 import './../assets/stylesheets/style.css';
 import './../assets/stylesheets/animate.css';
+import {doLogin} from './../api/authAPI';
+import SignUp from './Signup';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {login_success} from "../actions/login";
 
 class Landing extends Component {
 
     constructor(){
         super();
-        this.state={
-
+        this.state = {
+            email : "",
+            password : "",
+            message : "",
+            emailColor:""
         }
     }
 
@@ -26,6 +34,61 @@ class Landing extends Component {
 
     }
 
+    handleLogin = (() => {
+        // showAlert("SHowed Successful", "info", this);
+        document.getElementById('emailErr').innerHTML = '';
+        console.log('1',this.state.email);
+        console.log('1',this.state.password);
+        //Validation
+        let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+
+        if(!this.state.email){
+            document.getElementById('emailErr').innerHTML = 'Username is required';
+        }
+        else if (!this.state.password){
+            document.getElementById('passwordErr').innerText = 'Password is required';
+        }
+        else if(!re.test(this.state.email)){
+            document.getElementById('emailErr').innerHTML='Email is invalid';
+        }
+        else if (this.state.password.length > 0){
+            document.getElementById('passwordErr').innerText = '';
+
+            console.log('inside');
+            let payload = {
+                'email': this.state.email,
+                'password': this.state.password
+            };
+
+            doLogin(payload).then((response) => {
+                console.log(response.status);
+                if (response.status === 200) {
+                    response.json().then((data) => {
+                        console.log(data);
+                        this.props.login_success(data);
+                        this.props.history.push("/home");
+                    });
+
+                }
+                else if (response.status === 404) {
+                    this.setState({
+                        ...this.state,
+                        message: "User not registered. Please sign up"
+                    });
+                }
+                else if (response.status === 401) {
+                    this.setState({
+                        ...this.state,
+                        message: "Incorrect Password. Please try again"
+                    });
+                }
+                else {
+                    console.log("Error: ", response);
+                    // alert("Error while Signing In");
+                }
+            });
+        }
+    });
     redirectLogin = ((path)=>{
         this.props.history.push(path);
     });
@@ -38,11 +101,13 @@ class Landing extends Component {
 
                 <div className="form-group">
                     <input type="email" className="form-control" placeholder="Username" required="" />
+                    <span id="emailErr"/>
                 </div>
                 <div className="form-group">
                     <input type="password" className="form-control" placeholder="Password" required="" />
+                    <span id="passwordErr"/>
                 </div>
-                <button type="submit" className="btn btn-primary block full-width m-b">Login</button>
+                <button type="submit" className="btn btn-primary block full-width m-b" onClick={()=>{this.handleLogin()}}>Login</button>
 
                 <a href="#">
                     <small>Forgot password?</small>
